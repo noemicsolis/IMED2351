89ead26cb97851403f517d51314c66c0aa0495f6
noemisolis <noemicsolis@gmail.com>
2018-02-20 12:37:47 -0600

    Rubric 5
    event handlers and cookie added

--- list.js
+++ list.js
@@ -1,33 +1,35 @@
-var myDrinks= new Array();
-myDrinks[0] = "Macchiato,";
-myDrinks[1] = "Espresso,";
-myDrinks[2] = "Lattes,";
-myDrinks[3] = "Cappuccino,";
 
-document.write(myDrinks[2]);
 
-	<!-- Hide script from old browsers
+//Hide script from old browsers
 	// This script copyright 1997, Tom Negrino and Dori Smith.
 	// This script is from "JavaScript for the WWW, Visual QuickStart Guide, 2nd Ed."
 	// For more information, see <http://www.chalcedony.com/javascript/>.
 	// This script may be used and modified, but the copyright notice must remain intact.
 
-	now = new Date
+	var now = new Date();
 
 	if (now.getHours() < 5) {
-		document.write(" What are you doing up so late?");
+		document.write(" Shouldn't have drank so much ");
 	}
 	else if (now.getHours() < 10) {
 		document.write(" Good Morning! Time for Coffee!");
 	}
 	else if (now.getHours() < 11) {
-		document.write(" No sleeping in class!");
+		document.write(" I could go for a ");
 	}
 	else {
 		document.write(" Good Evening!");
 	}
 	
 	// End hiding script from old browsers -->
+var myDrinks= new Array();
+myDrinks[0] = "Macchiato,";
+myDrinks[1] = "Espresso,";
+myDrinks[2] = "Latte (:";
+myDrinks[3] = "Cappuccino,";
+
+document.write(myDrinks[2]);
+
 
 
 var userDrinks = prompt("How many cups of coffee do you drink in a day?");
@@ -47,8 +49,9 @@ if(userDrinks<3) {
 	}
 
 var myDemo = document.getElementById("demoparent");
+console.log("myDemo="+ myDemo);
 
-console.log(myDemo.childNodes.length);
+var listLength=myDemo.childNodes.length;
 
 var myDemoTags = document.getElementsByTagName("li");
 
@@ -72,3 +75,4 @@ var newText = document.createTextNode("Hey, this is new!");
 myDemo.appendChild(newItem);
 
 newItem.appendChild(newText);
+
