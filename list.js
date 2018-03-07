3cb44396929e8c9341e2ace1d372b090ea3bd704
noemisolis <noemicsolis@gmail.com>
2018-03-07 10:36:10 -0600

    Dynamic page
    clickable pics

--- list.js
+++ list.js
@@ -1,1 +1,28 @@
-			
+var noemi = document.getElementById("noemi");
+var kakashi = document.getElementById("kakashi");
+var photography = document.getElementById("photography");
+
+
+noemi.addEventListener("click", picLink);
+kakashi.addEventListener("click", picLink);
+photography.addEventListener("click", picLink);
+
+function picLink() {
+	var allImages = document.querySelectorAll("img");
+	
+	for (var i = 0; i <
+allImages.length; i++) {
+		allImages[i].className = "hide";
+	}
+	
+	var picId = this.attributes["data-img"].value;
+	var pic = document.getElementById(picId);
+	if (pic.className === "hide") {
+		pic.className = "";
+	} else {
+		pic.className = "hide";
+	}
+}
+
+
+
